47dfda3e3283a128ec6cbbf7dcd7f8cf7b72b316
Jake <jweber04005@msn.com>
2022-01-11 16:40:51 -0500

    Styled Homepage

--- frontend/src/components/SplashPage/StoresOverview.js
+++ frontend/src/components/SplashPage/StoresOverview.js
@@ -1,23 +1,26 @@
 import React from "react";
 
+
 function StoresOverview({ stores }) {
     return (
-        <div className="homepage-store-overview-container">
-            <h2>Popular Bars</h2>
-            {stores?.map((store) => {
-                return (
-                    <div key={store.id} className="homepage-store-card">
+        <>
+            <h2 className="homepage-h2">Popular Bars</h2>
+            <div className="homepage-store-overview-container">
+                {stores?.map((store) => {
+                    return (
+                        <div key={store.id} className="homepage-store-card">
 
-                        <div className="stores-info-container">
-                            <h3 className="store-name">{store.name}</h3>
-                            <h4 className="store-description">{store.description}</h4>
-                            <h4 className="store-location">{store.location}</h4>
+                            <div className="homepage-stores-info-container">
+                                <h3 className="homepage-store-name">{store.name}</h3>
+                                <h4 className="homepage-store-description">{store.description}</h4>
+                                <h4 className="homepage-store-location">{store.location}</h4>
+                            </div>
+
                         </div>
-
-                    </div>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
 
