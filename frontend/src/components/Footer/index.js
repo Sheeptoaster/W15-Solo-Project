ea271dad8196e2d2e7c99e51e37db19c5c4fb653
Jake <jweber04005@msn.com>
2022-01-13 19:35:22 -0500

    Did not Link to my LinkedIn

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -14,7 +14,7 @@ function Footer() {
                     <a className="footer-links" href='https://github.com/Sheeptoaster/W15-Solo-Project'>
                         Github
                     </a>
-                    <a className="footer-links" href='https://www.linkedin.com/feed/'>
+                    <a className="footer-links" href='https://www.linkedin.com/in/jacob-weber-662a08153/'>
                         LinkedIn
                     </a>
                 </div>
